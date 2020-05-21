254bf77426ba92881f8837024b01554a58ff05a9
kynmh69 <hiroki.kayanuma@gmail.com>
2020-05-21 21:50:14 +0900

    initial commit

---
+++ Calcuration/Calcuration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calcuration
+//
+//  Created by Hiroki Kayanuma on 2020/05/21.
+//  Copyright © 2020 Hiroki Kayanuma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
